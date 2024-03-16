721a70d2c8e7de7b5e569dd44c056e221c199e64
Your Name <phamthanhcong0709@gmail.com>
2024-03-16 22:27:40 +0700

    day1

---
+++ chuHoachuThuong.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        char n;
+        cin >> n;
+        if ((int) n >= 97)
+        {
+            n -= 32;
+            cout << n << endl;
+        }
+        else
+        {
+            n += 32;
+            cout << n << endl;
+        }
+            
+    }
+}
---
+++ tong1denN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        long long sum = (long long) ((long long)(1+n)*n)/2;
+        cout << sum << endl;
+    }
+}
